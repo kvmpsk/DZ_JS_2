9762f139c3d34d4b7eb3478b15d4bc3b0efea4e8
VKosterin <kvm4000@gmail.com>
2021-04-23 19:39:15 +0300

    final

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -4,20 +4,22 @@ const products = [
     {id: 2, title: 'Mouse', price: 1500},
     {id: 3, title: 'Keyboard', price: 5000},
     {id: 4, title: 'Gamepad', price: 4500},
+    {id: 5, title: 'Mobile', price: 30000},
+    {id: 6, title: 'Tablet', price: 16000},
+    {id: 7, title: 'Xerox PE220', price: 12000},
+    {id: 8, title: 'Scanner', price: 5000},
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <img class="product-img" src="#" alt="Фото товара">
-                <h3 class="product-title">${title}</h3>
-                <p class="product-price">${price}</p>
+const renderProduct = (item, img) => `<div class="product-item">
+                <img class="product-img" src="${img}" alt="Фото товара">
+                <h3 class="product-title">${item.title}</h3>
+                <p class="product-price">${item.price}</p>
                 <button class="by-btn">Добавить в корзину</button>
             </div>`;
-}
 
-const renderProducts = (list) => {
-    let productListHTML = list.map((item) => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productListHTML;
+
+const renderProducts = list => {
+   document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(""));
 }
 
 renderProducts(products);
